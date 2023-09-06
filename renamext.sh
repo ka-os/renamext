f089a7dbf1198ce21cc7ca85b6f285070fe710d4
ka-os <144249152+ka-os@users.noreply.github.com>
2023-09-06 19:33:06 +0200

    Add files via upload

--- renamext.sh
+++ renamext.sh
@@ -1,7 +1,8 @@
- #!/bin/bash
+#!/bin/bash
 
-echo 'Renaming files extension :: *.'$1 '-> *.'$2
-echo ' '
+echo -e 'Renaming files extension :: *.'$1 '-> *.'$2'\n'
+
+
 for file in *.$1; do
 	mv -- "$file" "${file%.$1}.$2"
 	echo "Renaming extension of $file ->" "${file%.$1}.$2"
